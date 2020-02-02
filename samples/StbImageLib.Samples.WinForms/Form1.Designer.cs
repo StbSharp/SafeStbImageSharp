45eee3434f3211af27880f1e3a8c9a654d19ad7b
Roman Shapiro <eleroman@mail.ru>
2020-02-02 23:13:13 +0700

    Added WinForms sample

---
+++ samples/StbImageLib.Samples.WinForms/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace StbSharp.WinForms.Test
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this._numericWidth = new System.Windows.Forms.NumericUpDown();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this._numericHeight = new System.Windows.Forms.NumericUpDown();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this._numericWidth)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this._numericHeight)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(13, 13);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Load...";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.pictureBox1.Location = new System.Drawing.Point(12, 42);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(707, 491);
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// _numericWidth
+			// 
+			this._numericWidth.Location = new System.Drawing.Point(429, 13);
+			this._numericWidth.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+			this._numericWidth.Name = "_numericWidth";
+			this._numericWidth.Size = new System.Drawing.Size(120, 20);
+			this._numericWidth.TabIndex = 7;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(385, 15);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(38, 13);
+			this.label1.TabIndex = 8;
+			this.label1.Text = "Width:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(555, 15);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(41, 13);
+			this.label2.TabIndex = 10;
+			this.label2.Text = "Height:";
+			// 
+			// _numericHeight
+			// 
+			this._numericHeight.Location = new System.Drawing.Point(599, 13);
+			this._numericHeight.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+			this._numericHeight.Name = "_numericHeight";
+			this._numericHeight.Size = new System.Drawing.Size(120, 20);
+			this._numericHeight.TabIndex = 9;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(731, 545);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this._numericHeight);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this._numericWidth);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this._numericWidth)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this._numericHeight)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.NumericUpDown _numericWidth;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.NumericUpDown _numericHeight;
+	}
+}
+
---
+++ samples/StbImageLib.Samples.WinForms/Form1.cs
@@ -0,0 +1,84 @@
+﻿using StbImageLib;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace StbSharp.WinForms.Test
+{
+	public partial class Form1 : Form
+	{
+		private string _fileName;
+		private ImageResult _loadedImage;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				using (var dlg = new OpenFileDialog())
+				{
+					dlg.Filter =
+						"PNG Files (*.png)|*.png|JPEG Files (*.jpg)|*.jpg|BMP Files (*.bmp)|*.bmp|PSD Files (*.psd)|*.psd|TGA Files (*.tga)|*.tga|GIF Files (*.gif)|*.gif|All Files (*.*)|*.*";
+					if (dlg.ShowDialog() != DialogResult.OK)
+					{
+						return;
+					}
+
+					_fileName = dlg.FileName;
+
+					var bytes = File.ReadAllBytes(_fileName);
+
+					using (var stream = File.OpenRead(_fileName))
+					{
+						_loadedImage = ImageResult.FromStream(stream, ColorComponents.RedGreenBlueAlpha);
+					}
+					SetImage();
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Error", ex.Message);
+			}
+		}
+
+		private void SetImage()
+		{
+			// Convert to bgra
+			var data = new byte[_loadedImage.Data.Length];
+			Array.Copy(_loadedImage.Data, data, data.Length);
+
+			for (var i = 0; i < _loadedImage.Width*_loadedImage.Height; ++i)
+			{
+				var r = data[i*4];
+				var g = data[i*4 + 1];
+				var b = data[i*4 + 2];
+				var a = data[i*4 + 3];
+
+
+				data[i*4] = b;
+				data[i*4 + 1] = g;
+				data[i*4 + 2] = r;
+				data[i*4 + 3] = a;
+			}
+
+			// Convert to Bitmap
+			var bmp = new Bitmap(_loadedImage.Width, _loadedImage.Height, PixelFormat.Format32bppArgb);
+			var bmpData = bmp.LockBits(new Rectangle(0, 0, _loadedImage.Width, _loadedImage.Height), ImageLockMode.WriteOnly,
+				bmp.PixelFormat);
+
+			Marshal.Copy(data, 0, bmpData.Scan0, bmpData.Stride*bmp.Height);
+			bmp.UnlockBits(bmpData);
+
+			pictureBox1.Image = bmp;
+			_numericWidth.Value = _loadedImage.Width;
+			_numericHeight.Value = _loadedImage.Height;
+		}
+	}
+}
